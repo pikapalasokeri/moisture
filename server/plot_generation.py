a3db8971f76f4f0ab1e285d6ff3c80a46eff76cb
pikapalasokeri <5081567+pikapalasokeri@users.noreply.github.com>
2021-11-15 20:47:41 +0100

    server: Add debug printouts and fix bug in plot generation

--- server/plot_generation.py
+++ server/plot_generation.py
@@ -16,18 +16,25 @@ def generate_plot(es, start_datetime, end_datetime):
     locations = es.get_locations()
     print("Got locations:", locations)
     for location in locations:
+        print("Location:", location)
         sensors = es.get_sensors(location)
+        print("sensors:", sensors)
         series = {sensor_id: ([], []) for sensor_id in sensors}
+        print("series:", series)
         series_timestamps = {sensor_id: [] for sensor_id in sensors}
+        print("series timestamps:", series_timestamps)
         for r in readings:
             if r.sensor_id in sensors and r.location == location:
+                print("Got reading:", r)
                 series[r.sensor_id][0].append(r.timestamp)
                 series[r.sensor_id][1].append(r.raw_value)
 
         for sensor_id, serie in series.items():
-            timestamps, values = zip(*sorted(zip(serie[0], serie[1])))
-            plt.plot(timestamps, values, ".-", label=f"{location}/{sensor_id}")
-            print("Plotted serie:", location, sensor_id)
+            print(sensor_id, serie)
+            if serie[0]:
+                timestamps, values = zip(*sorted(zip(serie[0], serie[1])))
+                plt.plot(timestamps, values, ".-", label=f"{location}/{sensor_id}")
+                print("Plotted serie:", location, sensor_id)
 
     formatter = mdates.DateFormatter(
         "%Y-%m-%d %H:%M", tz=pytz.timezone("Europe/Stockholm")
