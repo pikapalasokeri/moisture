4506cd8dcb40ab62d6b8a5e2d028c1fd5b114be9
pikapalasokeri <5081567+pikapalasokeri@users.noreply.github.com>
2022-06-28 21:04:53 +0200

    server: Add snap bin to PATH in up script

--- server/up.sh
+++ server/up.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+export PATH="${PATH}:/snap/bin"
+
 DOCKER_BUILDKIT=1 docker build --file web_server.Dockerfile --tag web_server .
 DOCKER_BUILDKIT=1 docker build --file data_receiver.Dockerfile --tag data_receiver .
 
